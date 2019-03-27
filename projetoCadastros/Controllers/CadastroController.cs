4cf14475b60eab1721eb0b20aac20ea2fe821b83
Nathália Moreira Spinula Costa <nathaliaspinula@gmail.com>
2019-03-26 21:20:24 -0300

    Inicio do Projeto: dois controllers e uma view
    Foi criado os controllers Home e Cadastro (estao vazios) e a view de
    Cadastro
    
    Signed-off-by: Nathália Moreira Spinula Costa <nathaliaspinula@gmail.com>

---
+++ projetoCadastros/Controllers/CadastroController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projetoCadastros.Controllers
+{
+    public class CadastroController : Controller
+    {
+        //
+        // GET: /Cadastro/
+
+        public string Index()
+        {
+            return ("<script>alert('oi')</script>");
+        }
+    }
+}
---
+++ projetoCadastros/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projetoCadastros.MvcApplication" Language="C#" %>
