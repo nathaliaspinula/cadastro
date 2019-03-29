c8b35347edbd6badcf26e2b98302d8b4eb33c596
Nathália Moreira Spinula Costa <nathaliaspinula@gmail.com>
2019-03-29 01:40:54 -0300

    Projeto quase completo, só falta o editar

---
+++ projetoCadastros/Controllers/ClienteController.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using projetoCadastros.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projetoCadastros.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Cadastro");
+        }
+
+        public ActionResult IndexClientes()
+        {
+            var cliente = new Cliente();
+            var clientes = cliente.ListarClientes().Where(c => c.Status == "A").ToList();
+            return View("Cliente",clientes);
+        }
+        public ActionResult Criar(FormCollection collection)
+        {
+            // Adiciona no ViewBag a mensagem que será exibida quando houver um cadastro realizado
+            ViewBag.Cadastro = "Cadastro realizado com sucesso!";
+
+            var cliente = new Cliente();
+
+            cliente.AdicionaCliente(collection);
+
+            return RedirectToAction("../Home/Index");
+        }
+        public ActionResult ConsultaCep(string cep)
+        {
+            string url = "http://viacep.com.br/ws/" + cep + "/json/";
+
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(url);
+            StreamReader readStream = new StreamReader(myReq.GetResponse().GetResponseStream());
+
+            string data = readStream.ReadToEnd();
+
+            readStream.Close();
+
+            return Json(data);
+        }
+
+        public ActionResult ExcluirCliente(string codCliente)
+        {
+            ViewBag.Cadastro = "Excluído com sucesso";
+
+            var cliente = new Cliente();
+
+            var clientes = cliente.ListarClientes();
+
+            var clienteExclusao = clientes.Where(c => c.Id == codCliente).First();
+
+            cliente.AtualizaDados(clienteExclusao);
+
+            return View("~/Views/Home/Index.cshtml");
+        }
+
+        public ActionResult EditarCliente(string codCliente)
+        {
+            var cliente = new Cliente();
+
+            var clientes = cliente.ListarClientes();
+
+            var clienteAlterar = clientes.Where(c => c.Id == codCliente).First();
+
+            return View("Editar",clienteAlterar);
+        }
+
+        public ActionResult Editar(FormCollection collection)
+        {
+            ViewBag.Cadastro = "Alterado com sucesso";
+
+            var cliente = new Cliente();
+
+            cliente.EditarDados(collection);
+
+            return View("~/Views/Home/Index.cshtml");
+        }
+
+    }
+}
---
+++ projetoCadastros/Models/Cliente.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projetoCadastros.Models
+{
+    public class Endereco
+    {
+        [Key]
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string Bairro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Status { get; set; }
+    }
+    public class Cliente
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Status { get; set; }
+        public List<Endereco> Enderecos { get; set; }
+
+        public void AdicionaCliente(FormCollection collection)
+        {
+            var path = System.Web.HttpContext.Current.Server.MapPath("~/Models/Dados/dados.txt");
+
+            string text = System.IO.File.ReadAllText(path, Encoding.UTF8);
+
+            List<Cliente> clientes = (!String.IsNullOrEmpty(text)) ? JsonConvert.DeserializeObject<List<Cliente>>(text) : new List<Cliente>();
+
+            var enderecos = new List<Endereco>();
+
+            for (int i = 1; !String.IsNullOrEmpty(collection["Cep_" + i]); i++)
+            {
+                enderecos.Add(new Endereco
+                {
+                    Bairro = collection["Bairro_" + i],
+                    Cep = collection["Cep_" + i],
+                    Cidade = collection["Cidade_" + i],
+                    Complemento = collection["Complemento_" + i],
+                    Logradouro = collection["Logradouro_" + i],
+                    Numero = collection["Numero_" + i],
+                    UF = collection["UF_" + i],
+                    Status = "A"
+                });
+            }
+
+            var cliente = new Cliente
+            {
+                Id = Guid.NewGuid().ToString(),
+                Nome = collection["Nome"],
+                Telefone = collection["Telefone"],
+                Status = "A",
+                Enderecos = enderecos
+            };
+
+            while (clientes.Any(c => c.Id == cliente.Id))
+            {
+                cliente.Id = Guid.NewGuid().ToString();
+            }
+
+            clientes.Add(cliente);
+
+            var strObj = JsonConvert.SerializeObject(clientes); ;
+
+            File.WriteAllText(path, strObj);
+
+        }
+        internal void AtualizaDados(Cliente clienteExclusao)
+        {
+            var path = System.Web.HttpContext.Current.Server.MapPath("~/Models/Dados/dados.txt");
+
+            string text = System.IO.File.ReadAllText(path, Encoding.UTF8);
+
+            var clientes = ListarClientes();
+
+            clientes.FirstOrDefault(c => c.Id == clienteExclusao.Id).Status = "E";
+
+            var strObj = JsonConvert.SerializeObject(clientes); ;
+
+            File.WriteAllText(path, strObj);
+        }
+
+        internal void EditarDados(FormCollection collection)
+        {
+            var path = System.Web.HttpContext.Current.Server.MapPath("~/Models/Dados/dados.txt");
+
+            string text = System.IO.File.ReadAllText(path, Encoding.UTF8);
+
+            var clientes = ListarClientes();
+
+            var clienteEditar = clientes.FirstOrDefault(c => c.Id == collection["ID"]);
+
+            var strObj = JsonConvert.SerializeObject(clientes);
+
+            File.WriteAllText(path, strObj);
+        }
+
+
+        public List<Cliente> ListarClientes()
+        {
+            var path = System.Web.HttpContext.Current.Server.MapPath("~/Models/Dados/dados.txt");
+
+            string text = System.IO.File.ReadAllText(path, Encoding.UTF8);
+
+            List<Cliente> clientes = (!String.IsNullOrEmpty(text)) ? JsonConvert.DeserializeObject<List<Cliente>>(text) : new List<Cliente>();
+
+            return clientes;
+        }
+    }
+}
